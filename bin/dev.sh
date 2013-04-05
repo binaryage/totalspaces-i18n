3229abc5a14922cb08b3282c17ad33b2d6c60b75
Stephen Sykes <sdsykes@gmail.com>
2013-04-05 16:47:08 +0300

    Improve dev and undev - only link the particular language you are working on

--- bin/dev.sh
+++ bin/dev.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+CHOSENLANG=$1
+
+if [ -z "$CHOSENLANG" ]; then
+  echo "Language specifier missing"
+  exit
+fi
+
 TOTALSPACES_RESOURCES='/Applications/TotalSpaces.app/Contents/Resources'
-TOTALSPACES_RESOURCES_BACKUP='/Applications/TotalSpaces.app/Contents/ResourcesOrig'
 
 # need absolute path of the repo's root
 pushd . > /dev/null
@@ -14,23 +21,25 @@ if [ ! -d "$TOTALSPACES_RESOURCES" ]; then # is it a folder?
   exit
 fi
 
-if [ -L "$TOTALSPACES_RESOURCES" ]; then # is is a symlink?
-  echo "TotalSpaces is already in dev mode. Folder '$TOTALSPACES_RESOURCES' is a symlink! Exiting.".
+TS_LANG_FOLDER=$TOTALSPACES_RESOURCES/$CHOSENLANG.lproj
+TS_LANG_FOLDER_BACKUP=$TOTALSPACES_RESOURCES/$CHOSENLANG.lproj.backup
+LOCAL_LANG_FOLDER=$ROOT/app/$CHOSENLANG.lproj
+
+if [ ! -d "$LOCAL_LANG_FOLDER" ]; then
+  mkdir "$LOCAL_LANG_FOLDER"
+  cp "$ROOT/app/en.lproj/"* "$LOCAL_LANG_FOLDER"
+fi
+
+if [ -L "$TS_LANG_FOLDER" ]; then # is is a symlink?
+  echo "TotalSpaces is already in dev mode for this language. Folder '$TS_LANG_FOLDER' is a symlink! Exiting.".
   popd > /dev/null
   exit
 fi
 
 # ok, we should be safe to do the replacement
-sudo mv "$TOTALSPACES_RESOURCES" "$TOTALSPACES_RESOURCES_BACKUP"
-sudo ln -s "$ROOT/app" "$TOTALSPACES_RESOURCES"
-sudo cp "$TOTALSPACES_RESOURCES_BACKUP/"*.nib "$TOTALSPACES_RESOURCES"
-sudo cp "$TOTALSPACES_RESOURCES_BACKUP/"*.png "$TOTALSPACES_RESOURCES"
-sudo cp "$TOTALSPACES_RESOURCES_BACKUP/"*.plist "$TOTALSPACES_RESOURCES"
-sudo cp "$TOTALSPACES_RESOURCES_BACKUP/"*.html "$TOTALSPACES_RESOURCES"
-sudo cp "$TOTALSPACES_RESOURCES_BACKUP/"*.icns "$TOTALSPACES_RESOURCES"
-sudo cp "$TOTALSPACES_RESOURCES_BACKUP/"*.pem "$TOTALSPACES_RESOURCES"
-sudo cp "$TOTALSPACES_RESOURCES_BACKUP/"*.applescript "$TOTALSPACES_RESOURCES"
-sudo cp "$TOTALSPACES_RESOURCES_BACKUP/"*.scpt "$TOTALSPACES_RESOURCES"
-sudo cp -r "$TOTALSPACES_RESOURCES_BACKUP/"*.app "$TOTALSPACES_RESOURCES"
+if [ -d "$TS_LANG_FOLDER" ]; then
+  sudo mv "$TS_LANG_FOLDER" "$TS_LANG_FOLDER_BACKUP"
+fi
+sudo ln -s "$LOCAL_LANG_FOLDER" "$TS_LANG_FOLDER"
 
 popd > /dev/null
--- bin/undev.sh
+++ bin/undev.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
+CHOSENLANG=$1
+
+if [ -z "$CHOSENLANG" ]; then
+  echo "Language specifier missing"
+  exit
+fi
+
 TOTALSPACES_RESOURCES='/Applications/TotalSpaces.app/Contents/Resources'
-TOTALSPACES_RESOURCES_BACKUP='/Applications/TotalSpaces.app/Contents/ResourcesOrig'
+
+# need absolute path of the repo's root
+pushd . > /dev/null
+cd "$(dirname "$0")"
+cd ..
+ROOT=$PWD
+
+TS_LANG_FOLDER=$TOTALSPACES_RESOURCES/$CHOSENLANG.lproj
+TS_LANG_FOLDER_BACKUP=$TOTALSPACES_RESOURCES/$CHOSENLANG.lproj.backup
+LOCAL_LANG_FOLDER=$ROOT/app/$CHOSENLANG.lproj
 
 if [ -d "$TOTALSPACES_RESOURCES" ]; then # is it a folder?
-  if [ -L "$TOTALSPACES_RESOURCES" ]; then # is it a symlink?
-    sudo rm "$TOTALSPACES_RESOURCES"
-    sudo mv "$TOTALSPACES_RESOURCES_BACKUP" "$TOTALSPACES_RESOURCES"
+  if [ -L "$TS_LANG_FOLDER" ]; then # is it a symlink?
+    sudo rm "$TS_LANG_FOLDER"
+    if [ -d "$TS_LANG_FOLDER_BACKUP" ]; then
+      sudo mv "$TS_LANG_FOLDER_BACKUP" "$TS_LANG_FOLDER"
+    fi
     exit
   fi
 fi
